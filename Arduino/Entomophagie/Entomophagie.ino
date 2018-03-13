0c766e6846a1b82ce3321d03b8a9199bcc13108d
ftipotsch <ftipotsch@tsn.at>
2018-03-13 07:43:01 +0100

    Arduion + Text

---
+++ Arduino/Entomophagie/Entomophagie.ino
@@ -0,0 +1,166 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+//temperature
+//DHT Sensor
+#include<stdlib.h>
+#include "DHT.h"
+
+#define DHTPIN 2     // what digital pin we're connected to
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE, 15);
+
+//Adafruit
+#include "Adafruit_CCS811.h"
+Adafruit_CCS811 ccs;
+
+
+
+
+
+//light sensor
+int photocellPin = 1; // the cell and 10K pulldown are connected to a0
+int photocellReading; // the analog reading from the sensor divider
+int LEDpin = 11; // connect Red LED to pin 11 (PWM pin) (this is not needed to use the sensor but if you have one laying around it could add a cool affect! This can be bought at your local Radio Shack.)
+int LEDbrightness; //
+
+
+
+
+//WIFI
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "yawn";
+const char* password = "enlu9505";
+const char* host = "192.168.43.246";
+const char* streamId   = "/swpp/entomophagie/webapp/api/web/v1/datas";
+
+
+void setup() {
+ Serial.begin(115200); 
+ dht.begin();
+
+ //Adafruit
+ Serial.println("CCS811 test");
+  if(!ccs.begin()){
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    //while(1);  
+    }
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+
+void loop() {
+
+// Adafruit
+delay(2000);
+  if(ccs.available()){
+   // float temp = ccs.calculateTemperature();
+    if(!ccs.readData()){
+      Serial.print("CO2: ");
+      Serial.println(ccs.geteCO2());
+     }
+    else{
+      Serial.println("ERROR!");
+      //while(1);
+    }
+  }
+//DHT
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature(true);
+
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    //return;
+  }
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+
+    
+  
+// Light Reading 
+   photocellReading = analogRead(A0);
+   Serial.print("Light = ");
+   Serial.println(photocellReading);
+ 
+   delay(3000); 
+//WIFI
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "";
+  url += streamId;
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  
+  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  
+  // Read all the lines of the reply from server and print them to Serial
+  
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+
+
+
+
+}
+
+
+
