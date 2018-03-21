29e9cd106eabb3e649d693a2af0d0b3a7e569dcf
ftipotsch <ftipotsch@tsn.at>
2018-03-21 11:24:34 +0100

    Work

--- Arduino/ProgrammCode/Entomophagie/Entomophagie.ino
+++ Arduino/ProgrammCode/Entomophagie/Entomophagie.ino
@@ -43,13 +43,6 @@ int LEDbrightness; //
 
 
 
-//WIFI
-#include <ESP8266WiFi.h>
-
-const char* ssid     = "TP-LINK_B0CD45";
-const char* password = "Petertrixiundco";
-const char* host = "192.168.178.29";
-const char* streamId   = "/swpp/entomophagie/webapp/api/web/v1/datas";
 
 
 void setup() {
@@ -76,22 +69,6 @@ void setup() {
   //Servo
   servo.attach(servoPin);
 
-  // We start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 
@@ -159,50 +136,9 @@ void loop() {
   Serial.print("X: ");
   Serial.println(xPosition);
       
-  //WIFI
-    Serial.print("connecting to ");
-    Serial.println(host);
-    
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-    const int httpPort = 80;
-    if (!client.connect(host, httpPort)) {
-      Serial.println("connection failed");
-      return;
-    }
-    
-    // We now create a URI for the request
-    String url = "";
-    url += streamId;
-    
-    Serial.print("Requesting URL: ");
-    Serial.println(url);
-    
-    // This will send the request to the server
-    
-    client.print(String("POST ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" + 
-                 "Connection: close\r\n\r\n");
-    
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-      if (millis() - timeout > 5000) {
-        Serial.println(">>> Client Timeout !");
-        client.stop();
-        return;
-      }
-    }
   
+
     
-    // Read all the lines of the reply from server and print them to Serial
-    
-    while(client.available()){
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
-    }
-    
-    Serial.println();
-    Serial.println("closing connection");
   
  }
 
